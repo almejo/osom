4464eef8f58f47583adb5f7a2df3846672cb15a5
alejo <alejandro.vera@gmail.com>
2017-12-12 17:57:55 -0300

    first version

--- src/main/java/com/almejo/osom/Emulator.java
+++ src/main/java/com/almejo/osom/Emulator.java
@@ -1,14 +1,26 @@
 package com.almejo.osom;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Emulator {
 
 	private static final int CYCLES = 10;
 	private DataBus bus = new DataBus();
 	private Z80Cpu cpu = new Z80Cpu(bus);
 
-	public void run() {
-		while (true) {
-			cpu.execute(CYCLES);
+	public void run() throws IOException {
+		byte[] bytes = Files.readAllBytes(Paths.get("/home/alejo/git/osom/roms/cpu_instrs.gb"));
+		System.out.println(bytes);
+
+		for (byte b : bytes) {
+			Integer i = b < 0 ? 256 + b: b;
+			System.out.println(Integer.toHexString(i));
+
 		}
+//		while (true) {
+//			cpu.execute(CYCLES);
+//		}
 	}
 }
