327760ed4361fb6f9738d28656be83c3a14acdb5
alejo <alejandro.vera@gmail.com>
2017-12-14 15:45:09 -0300

    Una ventana con el contenido del cartridge

--- src/main/java/com/almejo/osom/Emulator.java
+++ src/main/java/com/almejo/osom/Emulator.java
@@ -1,10 +1,11 @@
 package com.almejo.osom;
 
-import com.almejo.osom.cpu.ALU;
 import com.almejo.osom.cpu.Z80Cpu;
 import com.almejo.osom.memory.Cartridge;
 import com.almejo.osom.memory.MMU;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,22 +13,28 @@ import java.nio.file.Paths;
 
 public class Emulator {
 
-	private static final int CYCLES = 10;
-	private Z80Cpu cpu;
-
 	public void run(String file) throws IOException {
-		// Path path = Paths.get("/home/alejo/git/osom/roms/cpu_instrs.gb");
-//		Path path = Paths.get("/home/alejo/git/osom/roms/tetris.gb");
 		Path path = Paths.get(file);
 		byte[] bytes = Files.readAllBytes(path);
 		MMU mmu = new MMU();
 		Cartridge cartridge = new Cartridge(bytes);
 		System.out.println(cartridge);
 		mmu.addCartridge(cartridge);
-		cpu = new Z80Cpu();
+		Z80Cpu cpu = new Z80Cpu();
 		cpu.setMmu(mmu);
 		cpu.reset();
 
+		JTextArea textArea = new JTextArea();
+		textArea.setText(cartridge.toString());
+		textArea.setFont(new Font("monospaced", Font.PLAIN, 12));
+
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(new JScrollPane(textArea));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+
+		//noinspection InfiniteLoopStatement
 		while (true) {
 			cpu.execute();
 		}
