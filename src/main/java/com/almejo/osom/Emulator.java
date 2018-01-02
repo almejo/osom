ed943359a818518571305381dafa49ccff359549
alejo <alejandro.vera@gmail.com>
2018-01-02 10:24:25 -0300

    Better game sync

--- src/main/java/com/almejo/osom/Emulator.java
+++ src/main/java/com/almejo/osom/Emulator.java
@@ -14,9 +14,10 @@ import java.nio.file.Paths;
 
 public class Emulator {
 	private static final int CYCLES = 4194304;
-	private LCDScreen screen;
+	private static final int CYCLES_PER_FRAME = CYCLES / 60;
 
 	public void run(boolean bootBios, String file) throws IOException {
+
 		Path path = Paths.get(file);
 		GPU gpu = new GPU();
 		byte[] bytes = Files.readAllBytes(path);
@@ -30,6 +31,14 @@ public class Emulator {
 		gpu.setCpu(cpu);
 		cpu.reset(bootBios);
 
+		JFrame frame = new JFrame();
+		frame.setSize(160 * LCDScreen.FACTOR, 144 *  LCDScreen.FACTOR);
+		frame.setPreferredSize(new Dimension(160 *  LCDScreen.FACTOR, 144 *  LCDScreen.FACTOR));
+		LCDScreen screen = new LCDScreen(gpu);
+		frame.getContentPane().add(screen);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 
 //		JTextArea textArea = new JTextArea();
 //		textArea.setText(cartridge.toString());
@@ -40,31 +49,24 @@ public class Emulator {
 //		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //		frame.pack();
 //		frame.setVisible(true);
+
+		int time = 1000 / 60;
 		//noinspection InfiniteLoopStatement
-		JFrame frame = new JFrame();
-		frame.setSize(160 * 2, 144 * 2);
-		frame.setPreferredSize(new Dimension(160 * 2, 144 * 2));
-		screen = new LCDScreen(gpu);
-		frame.getContentPane().add(screen);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setVisible(true);
-		//noinspection InfiniteLoopStatement
-		int cyclesToScreen = CYCLES;
 		while (true) {
-			int oldCycles = cpu.clock.getT();
-			System.out.print(cpu.clock.getT() + " --> ");
-			cpu.execute();
-			int cycles = cpu.clock.getT() - oldCycles;
-			cpu.updateTimers(cycles);
-			cpu.checkInterrupts();
-			gpu.update(cycles);
-			cyclesToScreen -= cycles;
-			if (cyclesToScreen < 0) {
-				cyclesToScreen = CYCLES;
-				screen.invalidate();
-				screen.repaint();
+			long t = System.currentTimeMillis();
+			int cyclesToScreen = CYCLES_PER_FRAME;
+			while (cyclesToScreen > 0) {
+				int oldCycles = cpu.clock.getT();
+				System.out.print(cpu.clock.getT() + " --> ");
+				cpu.execute();
+				int cycles = cpu.clock.getT() - oldCycles;
+				cpu.updateTimers(cycles);
+				cpu.checkInterrupts();
+				gpu.update(cycles);
+				cyclesToScreen -= cycles;
 			}
+			long delta = System.currentTimeMillis() - t;
+			screen.repaint(time - delta);
 		}
 	}
 }
--- src/main/java/com/almejo/osom/LCDScreen.java
+++ src/main/java/com/almejo/osom/LCDScreen.java
@@ -8,29 +8,30 @@ import java.awt.*;
 public class LCDScreen extends JPanel {
 	private final GPU gpu;
 
-	private static final int FACTOR = 3;
+	static final int FACTOR = 2;
 
-	public LCDScreen(GPU gpu) {
+	LCDScreen(GPU gpu) {
 		setSize(new Dimension(160 * FACTOR, 144 * FACTOR));
 		this.gpu = gpu;
 	}
 
 	@Override
 	public void paint(Graphics g) {
-		Graphics2D g2 = (Graphics2D) g;
-		System.out.println("=====================================================================================");
-		g2.setColor(Color.yellow);
-		g2.drawRect(0, 0, 160 * FACTOR, 144 * FACTOR);
-		g2.setColor(Color.red);
-		g2.drawString("t: " +System.currentTimeMillis(), 0, 20);
+		Graphics2D graphics = (Graphics2D) g;
+		graphics.setColor(Color.yellow);
+		graphics.drawRect(0, 0, 160 * FACTOR, 144 * FACTOR);
+
 		int[][] pixels = gpu.getPixels();
 		for (int y = 0; y < 144; y++) {
 			for (int x = 0; x < 160; x++) {
 				Color color = getColor(pixels[x][y]);
-				g2.setColor(color);
-				g2.fillRect(x * FACTOR, y * FACTOR, FACTOR, FACTOR);
+				graphics.setColor(color);
+				graphics.fillRect(x * FACTOR, y * FACTOR, FACTOR, FACTOR);
 			}
 		}
+		graphics.setColor(Color.red);
+		graphics.drawString("t: " +System.currentTimeMillis(), 0, 10);
+		repaint();
 	}
 
 	private Color getColor(int i) {
